7379eb8b3c24611223ff5b0a7ee74ec38893b715
adamslack <adamslack@outlook.com>
2017-07-21 07:45:56 +0100

    Added some ToDo's

--- pipeline/scraper/retriever.js
+++ pipeline/scraper/retriever.js
@@ -5,11 +5,13 @@ const logger = require('./logger.js');
 const _ = require('lodash');
 const fs = require('fs');
 
+// TODO: Store App Data to the DB
 function store_app_data(app_data) {
     // push the app data to the DB
-    logger.info(app_data); // for now
+    logger.info(app_data); // for now   
 }
 
+// TODO Add Permission list to app Data JSON
 function fetch_app_data(search_term, number_of_apps, per_second) {
     gplay.search({
         term: search_term,
@@ -22,6 +24,7 @@ function fetch_app_data(search_term, number_of_apps, per_second) {
     );
 }
 
+// TODO: Query DB for Search Terms
 function fetch_search_terms() {
     // fetch from DB
 }
@@ -32,5 +35,14 @@ function open_search_terms(file_location) {
 
 _.forEach(
     open_search_terms(config.datadir + '/suggested_words.txt'),
-    (search_term) => fetch_app_data(search_term, 1, 1)
+    (search_term) => fetch_app_data(search_term, 4, 1)
 );
+
+
+// gplay.permissions({
+//     appId: 'com.dxco.pandavszombies',
+//     short: true
+// }).then(
+//     (app) => logger.info(app),
+//     (err) => logger.err(err.message)
+// );
