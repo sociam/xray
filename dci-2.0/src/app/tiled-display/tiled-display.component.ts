ece99d6a994b0709d10e61f1d72f5eb579af1548
electronic Max <max@hip.cat>
2017-08-15 00:45:07 +0100

    tiled display coming back to life

--- dci-2.0/src/app/tiled-display/tiled-display.component.ts
+++ dci-2.0/src/app/tiled-display/tiled-display.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tiled-display',
   templateUrl: './tiled-display.component.html',
-  styleUrls: ['./tiled-display.component.css']
+  styleUrls: ['./tiled-display.component.scss']
 })
 export class TiledDisplayComponent implements OnInit {
 
