5e83855d7892cdec61dd3d691f88e38fd41b86c3
electronic Max <max@hip.cat>
2017-08-16 23:38:32 +0100

    removed unnecessary files

--- dci-2.0/src/app/resizesvg.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ResizesvgDirective } from './resizesvg.directive';
-
-describe('ResizesvgDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ResizesvgDirective();
-    expect(directive).toBeTruthy();
-  });
-});
