32ceae9ac092d613350f805b26c60579b115f0e2
electronic Max <max@hip.cat>
2017-08-18 01:36:57 +0100

    got rid of glitchiness

--- dci-2.0/src/app/autocomplete/autocomplete.component.ts
+++ dci-2.0/src/app/autocomplete/autocomplete.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { LoaderService, APIAppInfo } from 'app/loader.service';
+import { sortBy, pullAllBy, differenceBy } from 'lodash';
 
 // thanks to http://4dev.tech/2016/03/tutorial-creating-an-angular2-autocomplete/ !
 
@@ -21,6 +22,7 @@ export class AutocompleteComponent implements OnInit, OnChanges {
   @Input() omit: APIAppInfo[];
   private _omitIDs: { [id: string]: boolean } = {};
   @Output() selectedChange = new EventEmitter<APIAppInfo>();
+  private nonce = '';
 
   constructor(private myElement: ElementRef, private loader: LoaderService) {
   }
@@ -28,7 +30,7 @@ export class AutocompleteComponent implements OnInit, OnChanges {
   ngOnInit() { }
   ngOnChanges(changes: SimpleChanges): void {
 
-    console.log('changes! ', changes);
+    // console.log('changes! ', changes, this.selected);
     
     if (changes.omit && this.omit) {
       this._omitIDs = this.omit.reduce((obj, a) => {
@@ -41,27 +43,47 @@ export class AutocompleteComponent implements OnInit, OnChanges {
     //   this.query = changes.selected.currentValue === undefined ? '' : changes.selected.currentValue.storeinfo.title;
     // }
 
-    this.query = this.selected !== undefined ? '' : this.selected.storeinfo.title;
-
+    this.query = ''; // this.selected === undefined ? '' : this.selected.storeinfo.title;
   }
 
   filter() {
     if (this.query.trim() !== '') {
+      let nonce = this.nonce = Math.round(1e12 * Math.random()).toString();
+
       this.loader.findApps(this.query.trim()).then((results: APIAppInfo[]) => {
-        const qL = this.query.toLowerCase().trim();
-        this.filteredList = results.filter((x) => !this._omitIDs[x.app] && x.storeinfo.title.toLowerCase().indexOf(qL) === 0 && x.icon);
-        console.log(`omit results > "${qL}"`, results.map(r => r.storeinfo.title), this.filteredList, this._omitIDs);
-        this.filteredList.sort((a, b) => {
-          let atitle = a.storeinfo.title.toUpperCase(); // ignore upper and lowercase
-          let btitle = b.storeinfo.title.toUpperCase(); // ignore upper and lowercase
-          if (atitle < btitle) {
-            return -1;
-          }
-          if (atitle > btitle) {
-            return 1;
-          }
-          return 0;
-        });
+
+        if (nonce !== this.nonce) { return; }
+
+        let qL = this.query.toLowerCase().trim(),
+              newL = results.filter((x) => !this._omitIDs[x.app] && x.storeinfo.title.toLowerCase().indexOf(qL) === 0 && x.icon),
+              by = (x) => x.storeinfo.title;        
+
+        let goners = differenceBy(this.filteredList, newL, by),
+          newbies = differenceBy(newL, this.filteredList, by);
+
+        // console.log('old filteredlist: ', this.filteredList.length, this.filteredList.map(a => a.storeinfo.title));
+        // console.log('goners: ', goners.length, goners.map(a => a.storeinfo.title));
+        // console.log('newbies ', newbies.length, newbies.map(a => a.storeinfo.title));
+
+        pullAllBy(this.filteredList, goners, by);
+        Array.prototype.splice.apply(this.filteredList, [this.filteredList, 0].concat(newbies));
+
+        // console.log('newlist ', this.filteredList.length, this.filteredList.map(a => a.storeinfo.title));
+        
+        this.filteredList = sortBy(this.filteredList, by);
+
+        // console.log(`omit results > "${qL}"`, results.map(r => r.storeinfo.title), this.filteredList, this._omitIDs);
+        // this.filteredList.sort((a, b) => {
+        //   let atitle = a.storeinfo.title.toUpperCase(); // ignore upper and lowercase
+        //   let btitle = b.storeinfo.title.toUpperCase(); // ignore upper and lowercase
+        //   if (atitle < btitle) {
+        //     return -1;
+        //   }
+        //   if (atitle > btitle) {
+        //     return 1;
+        //   }
+        //   return 0;
+        // });
       });
     } else {
       this.filteredList = [];
